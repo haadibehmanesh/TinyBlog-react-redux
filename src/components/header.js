0283ebfcc81c667a7474fc20caa012128b7ede64
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-05 08:31:01 +0200

    make header and contact componenets

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,29 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
 const Header = () => {
-    return ( 
-<>
+    return (
+        <>
+            <div className="navbar yellotail">
+                <Link to="/">
+                    <Navbar.Brand>The daily news</Navbar.Brand>
+                </Link>
 
-</>
+            </div>
+            <div className='nav'>
+                <div className='nav-item'>
+                    <Link to="/home">Home</Link>
+                </div>
+                <div className='nav-item'>
+                    <Link to="/contact">Contact</Link>
+                </div>
+            </div>
 
-     );
+
+        </>
+    )
 }
- 
+
 export default Header;
