9438ab9e572933b8bb69716b9e507f3b9cdf351c
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-06 20:07:26 +0200

    preparing action payloads for postAdded reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialStateValue = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
@@ -9,8 +9,19 @@ export const postsSlice = createSlice({
   name: "posts",
   initialState: initialStateValue,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content
+          }
+        }
+      }
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload;
