a279a276295dacc64ab589d828904446888a3f6c
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-06 10:11:24 +0200

    add Layout to wrap some components inside it and a route to show single post

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Contact from './components/contact';
 import Home from './components/home';
-import Layout from './components/layout';
+import SinglePost from "./features/posts/singlePost";
+import Layout from "./components/layout";
 
-const App = () => {
-    return (
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-       
-    );
+export default function App() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/posts/:postId" element={<SinglePost />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+
+  );
 }
-
-export default App;
