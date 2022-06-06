b2de23d729c3efcbb4f917fb3c1a5131a3d8523c
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-06 18:12:00 +0200

    adding a link for editpost

--- src/features/posts/singlePost.js
+++ src/features/posts/singlePost.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const SinglePost = () => {
   const { postId } = useParams();
@@ -21,6 +21,9 @@ const SinglePost = () => {
       <article className="post">
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
+        <Link to={`/editPost/${post.id}`} className="button">
+          Edit Post
+        </Link>
       </article>
     </section>
   )
