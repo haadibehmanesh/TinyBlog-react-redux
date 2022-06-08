e47234040547caeadd016e7039932232c22b584d
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-08 13:26:10 +0200

    encapsulate the rendering for one item in the list by extracting postExcerpt

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -5,7 +5,25 @@ import { PostAuthor } from './postAuthor'
 import { ReactionButtons } from './reactionButtons'
 import { TimeAgo } from './timeAgo'
 import { selectAllPosts, fetchPosts } from './postsSlice'
+import { Spinner } from '../../components/spinner'
 
+const PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
 export const PostsList = () => {
   const dispatch = useDispatch()
   // const posts = useSelector(state => state.posts)
@@ -13,11 +31,30 @@ export const PostsList = () => {
 
   const postStatus = useSelector(state => state.posts.status)
 
+  const error = useSelector(state => state.posts.error)
+
   useEffect(() => {
     if (postStatus === 'idle') {
       dispatch(fetchPosts())
     }
   }, [postStatus, dispatch])
+
+  let content
+
+  if (postStatus === 'loading') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map(post => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   // Sort posts in reverse chronological order by datetime string
   const orderedPosts = posts.slice().sort((a, b) => {
