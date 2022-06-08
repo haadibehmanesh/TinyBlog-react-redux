2b82ca2276722fd78230d4fe4641d4630d43b863
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-08 13:08:36 +0200

    using createAsyncThunk for postsSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 //import { sub } from 'date-fns'
 
 // const initialState = [
@@ -72,8 +73,8 @@ const postsSlice = createSlice({
       }
     },
     postUpdated(state, action) {
-      const { id, title, content } = action.payload
-      const existingPost = state.find((post) => post.id === id)
+      const { postId, title, content } = action.payload
+      const existingPost = state.posts.find(post => post.id === postId)
       if (existingPost) {
         existingPost.title = title
         existingPost.content = content
@@ -90,3 +91,8 @@ export const selectAllPosts = state => state.posts.posts
 
 export const selectPostById = (state, postId) =>
   state.posts.posts.find(post => post.id === postId)
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
