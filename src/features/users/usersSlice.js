1019471a80a156433dda52731e9c3d145ea755af
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-09 18:45:08 +0200

    change usersSlice to fetch data by axios

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from "axios";
 
-const initialState = [
-    { id: '0', name: 'Hadi Behmanesh' },
-    { id: '1', name: 'Reza Dashti' },
-    { id: '2', name: 'Ali Rezaei' }
-]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(USERS_URL);
+    return response.data
+})
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload;
+        })
+    }
 })
+
+export const selectAllUsers = (state) => state.users;
 
 export default usersSlice.reducer
