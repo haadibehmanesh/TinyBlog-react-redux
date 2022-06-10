cdd6a6b033abe152fc4b15bc6475d3e055eba3e8
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-10 11:44:10 +0200

    add updated post function and add extra reducer for it

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -20,6 +20,16 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
   const response = await axios.post(POSTS_URL, initialPost)
   return response.data
 })
+export const updatePost = createAsyncThunk('posts/updatePost', async (initialPost) => {
+  const { id } = initialPost;
+  try {
+      const response = await axios.put(`${POSTS_URL}/${id}`, initialPost)
+      return response.data
+  } catch (err) {
+      //return err.message;
+      return initialPost; // only for testing Redux!
+  }
+})
 
 
 const postsSlice = createSlice({
@@ -106,6 +116,17 @@ const postsSlice = createSlice({
         console.log(action.payload)
         state.posts.push(action.payload)
       })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+            console.log('Update could not complete')
+            console.log(action.payload)
+            return;
+        }
+        const { id } = action.payload;
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter(post => post.id !== id);
+        state.posts = [...posts, action.payload];
+    })
   },
 })
 
