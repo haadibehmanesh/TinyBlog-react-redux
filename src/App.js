191cf4f5c1d033a634c0a389c7ca5aeb35ad3e07
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-10 13:36:49 +0200

    add routes for userPage and userList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import SinglePost from "./features/posts/singlePost";
 import { EditPostForm } from "./features/posts/EditPostForm";
 import Layout from "./components/layout";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from "./components/home";
 import Contact from "./components/contact";
+import UsersList from "./features/users/UsersList";
+import UserPage from './features/users/UserPage';
 
 function App() {
   return (
@@ -14,6 +16,14 @@ function App() {
           <Route path=":postId" element={<SinglePost />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/* Catch all - replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
