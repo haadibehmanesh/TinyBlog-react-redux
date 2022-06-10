2884962810e2ea58361b697733e3f8fcaa5ef7da
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-10 13:39:15 +0200

    correct posts url

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -11,7 +11,7 @@ const UserPage = () => {
 
     const postTitles = postsForUser.map(post => (
         <li key={post.id}>
-            <Link to={`/post/${post.id}`}>{post.title}</Link>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
         </li>
     ))
 
