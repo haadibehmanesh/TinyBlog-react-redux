84e13d8d1b9a5ba54a41be97eacd1891e59e4549
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-10 09:56:45 +0200

    using extra reducer addNewPost instead of postAdded

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,31 +1,50 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
+//import { postAdded } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
+  const [body, setBody] = useState('')
   const [userId, setUserId] = useState('')
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
   const dispatch = useDispatch()
 
   const users = useSelector(state => state.users)
 
   const onTitleChanged = e => setTitle(e.target.value)
-  const onContentChanged = e => setContent(e.target.value)
+  const onContentChanged = e => setBody(e.target.value)
   const onAuthorChanged = e => setUserId(e.target.value)
 
-  const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId))
-      setTitle('')
-      setContent('')
+  
+  // const onSavePostClicked = () => {
+  //   if (title && content) {
+  //     dispatch(postAdded(title, content, userId))
+  //     setTitle('')
+  //     setContent('')
+  //   }
+  // }
+
+  // const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+  const canSave =
+    [title, body, userId].every(Boolean) && addRequestStatus === 'idle'
+
+  const onSavePostClicked = async () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus('pending')
+        await dispatch(addNewPost({ title, body, user: userId })).unwrap()
+        setTitle('')
+        setBody('')
+        setUserId('')
+      } catch (err) {
+        console.error('Failed to save the post: ', err)
+      } finally {
+        setAddRequestStatus('idle')
+      }
     }
   }
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
-
   const usersOptions = users.map(user => (
     <option key={user.id} value={user.id}>
       {user.name}
@@ -54,7 +73,7 @@ export const AddPostForm = () => {
         <textarea
           id="postContent"
           name="postContent"
-          value={content}
+          value={body}
           onChange={onContentChanged}
         />
         <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
